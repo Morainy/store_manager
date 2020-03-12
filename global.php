19c528489c028cdccc6818661c448d15c2c57586
wishchen(陈伟) <wishchen@tencent.com>
2020-03-12 12:24:43 +0800

    提交文件

---
+++ global.php
@@ -0,0 +1,14 @@
+<?php
+	$SERVER = "localhost";
+	$USERNAME = "root";
+	$PASSWD = "";
+	$DBNAME = "storage";
+	$PORT = 3306;
+
+	$USERS_TABLE = "users";
+	$STORE_TABLE = "store";
+
+	$SUCC = 0;
+	$ERR_USER_INVALID = -100;
+	$ERR_PWD_INVALID = -101;
+?>
---
+++ store_manager.php
@@ -0,0 +1,20 @@
+<?php
+	header("Content-type:text/html;charset=utf-8");
+	include("global.php");
+	// 创建连接
+	$conn = new mysqli($SERVER, $USERNAME, $PASSWD, $DBNAME, $PORT);
+	$conn->set_charset("utf8");
+	
+	$sql = "INSERT INTO " . $STORE_TABLE . " (product_type,brand,product_name,specs,quantity,in_price,out_price,memo) VALUES ('"
+			. $_POST["product_type"] . "','" . $_POST["brand"] . "','"
+			. $_POST["product_name"] . "','" . $_POST["specs"] . "','"
+			. $_POST["quantity"] . "','" . $_POST["in_price"] . "','"
+			. $_POST["out_price"] . "','" . $_POST["memo"] . "')";
+	echo "sql:" . $sql ."<br/>";
+    $result = $conn->query($sql);
+	if($result == false){
+        echo "<script charset='utf-8'>alert('录入失败');</script>";
+    } else {
+    	echo "<script charset='utf-8'>alert('录入成功');</script>";
+    }
+?>
