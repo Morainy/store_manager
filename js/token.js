97a0f7c48415d0433a189d3938f3894de311c8da
sachielxsuo <sachielxsuo@tencent.com>
2020-04-03 20:26:43 +0800

    增加rowspan

--- js/token.js
+++ js/token.js
@@ -1,10 +1,5 @@
 ;(function(win, $) {
-    var row_num = goodsTxtArr.length;
-    var sum = 0;
-    for (i = 0; i < goodsTxtArr.length; i++) { 
-        sum += goodsTxtArr[i].quantity_sum;
-    }
-    var template = `<% data.forEach(function(item, index) { %>
+    var template = `<% arr.forEach(function(item, index) { %>
         <tr>
             <td><%= item.id %></td>
             <td><%= item.product_type %></td>
@@ -14,21 +9,20 @@
             <td><%= item.quantity %></td>
             <td><%= item.out_price %></td>
             <td><%= item.quantity_sum %></td>
-            <td `;
-            if index == 0 {
-                template += `rowspan=`;
-                template += row_num;
-            }
-            template += `><%=`;
-            template += sum;
-            template += `%></td>`;
-            template += `<td><%= item.memo %></td>
+            <% if (index === 0) { %>
+                <td rowspan="<% data.length %>" ><%= sum %></td>
+            <% } %>
+            <td><%= item.memo %></td>
         </tr>
-    <% });
-    %>`;
+    <% }); %>`;
     
-    function render(data) {
-        var html = win.tmpl(template, {data});
+    function render(arr) {
+        var sum = data.reduce((total, curr) => total + curr, 0);
+        var html = win.tmpl(template, {
+            arr,
+            sum
+        });
+
         $('.my-table tbody').append(html);
     }
 
